1e3ba0a34c024209bb74fdca74894edded763305
Elena Bykova <elenabkv2@gmail.com>
2021-04-01 16:37:29 +0300

    Task 2

---
+++ Task_Lesson2/index.js
@@ -0,0 +1,19 @@
+console.log(121+'121' && !!(12+23+'Task'));
+console.log('15'*'Task' || !!('100'+70));
+console.log(15*'2'/6 && !'' && (!(26+2+'Task') || ('36'/6)));
+console.log('Eng'+123+'land' < 'Eng'+123+'Land');
+
+let x = 2;
+let y = "3";
+console.log(!(+y+'Task') || ++x ** 3+5)
+
+let z = 10;
+let r = 4;
+console.log(19%4 + 3 === (!!'' || -z%4 + 8))
+
+console.log(null == undefined && y ** 2 || !(15 + 3 + "Task"))
+console.log(--z / ++r  + + '123' && (20%3 + 5 + 'Bar')>((4-2) ** 2 + 3 + 'bar'))
+console.log(!!(3 + +'true') || undefined!=null || (3 + + true))
+console.log(!' ' === 'RePI1ster'>'ReРI1ster')
+console.log(23%5 + +'123' + 'Task' <= 5 ** 3 + +'true' + 'task')
+console.log(!('undefined' + 'null') || +'task')
